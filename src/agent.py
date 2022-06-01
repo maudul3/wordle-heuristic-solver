e46fcde7b37836891f639227553d9ffc34b44a11
Drew Mahler <drewmahler@Drews-MacBook-Pro-2.local>
2022-06-01 01:16:45 -0700

    Added in base functionality for guess to reduce the search space by elimination. Seeing some exciting results in the main program

--- src/agent.py
+++ src/agent.py
@@ -2,9 +2,9 @@ class Agent:
     def __init__(self, word_list):
         # Deep copy of the word list
         self.word_list = list(word_list)
-        # self.letter_list = [] - possible use?
+        self.most_recent_guess = None
     
-    def guess(self, constraints, heuristic):
+    def guess(self, constraints, heuristic=None):
         """
         Args:
             constraints (list<str>): constraints on word list, represented by
@@ -16,11 +16,29 @@ class Agent:
         # letter in word but not in position
         # letter in word and in correct position
         # letter not in word
-        # 'bread' = '10210'
+        # 'bread' = 10210'
         # 2 - correct position 1 - in word wrong position 0 - not in word
         # edge case - 2 of the same letters
 
         # Reduce word list based on existing constraints
+        for idx, constraint in enumerate(constraints):
+            reference_character = self.most_recent_guess.rep[idx]
+
+            if constraint == '2':
+                self.word_list = [
+                    word for word in self.word_list 
+                    if word.rep[idx] == reference_character
+                ]
+            elif constraint == '1':
+                self.word_list = [
+                   word for word in self.word_list 
+                   if word.rep[idx] != reference_character and reference_character in word.rep
+                ]
+            elif constraint == '0':
+                self.word_list = [
+                   word for word in self.word_list 
+                   if reference_character not in word.rep
+                ]
 
         # Decide if it uses exploration route?
         # Consider words not in word list? Needs attribute in that case
@@ -30,10 +48,23 @@ class Agent:
             x = 1
         elif heuristic == 'word':
             x = 1
-        else:
-            raise ValueError('Undefined heuristic')
 
         # Choose word at front of word list
+        self.most_recent_guess = self.word_list[0]
 
-        return self.word_list[0]
-
+        return self.most_recent_guess
+        
+if __name__ == '__main__':
+    from word import Word
+    from wordlegame import WordleGame
+    from word_lists import agent_list
+    test = Word("sword")
+    agent = Agent(agent_list)
+    game = WordleGame(test)
+    constraints = []
+    while game.guesses_remaining and not game.correct_guess:
+        guess = agent.guess(constraints, "letter")
+        print ("Guess :", guess.rep)
+        constraints = game.check_guess(guess)
+        print ("Constraints: ", constraints)
+    
--- src/main.py
+++ src/main.py
@@ -1,46 +1,65 @@
 from word_lists import agent_list, test_list
 from agent import Agent
 from wordlegame import WordleGame, STARTING_GUESSES
+from random import random
 
 
 if __name__ == '__main__':
 
     # Can likely make the below two blocks their own functions
-    heuristic = 'letter'
-    letter_time_to_guess = []
+    no_heuristic_time_to_guess = []
     # Possibly: subset the test list for compute time
         # if we subset the list, make sure words aren't repeated
     for word in test_list:
         agent = Agent(agent_list)
         game = WordleGame(word)
-        constraints = None
+        constraints = []
+        print_game = random()
+        print_percentage = 0.05
+        if print_game < print_percentage:
+            print ("Game for test word: ", word.rep )
         while game.guesses_remaining and not game.correct_guess:
-            guess = agent.guess(constraints, heuristic)
+            guess = agent.guess(constraints)
             constraints = game.check_guess(guess)
+            if print_game < print_percentage:
+                print ("Guess: ", guess.rep)
+                print ("Constraints: ", constraints)
+
 
         # Add to number of guesses list for data analysis
         # Use of correct_guess is to distinguish between
         # accurate and inaccurate final guesses
         # Values in list 1-6 == correctly guessed &
         # 7 == did not correctly guess
-        letter_time_to_guess.append(
+        no_heuristic_time_to_guess.append(
             (STARTING_GUESSES + (not game.correct_guess)) 
             - game.guesses_remaining
         )
-
         # Add file output for our time-to-guess lists
         # Possibly: list of words that were guessed correctly
         # Possibly: list of words that were not guessed
 
         # 2 list that is number of guesses [1-7, 1-7, 1-7]
         # one for each heuristics
-    
+    correct_guesses = sum(
+        [1 if guess_number <= 6 else 0 for guess_number in no_heuristic_time_to_guess]
+    )
+    no_correct_guesses = sum(
+        [1 if guess_number == 7 else 0 for guess_number in no_heuristic_time_to_guess] 
+    )
+
+    print ("Total games: ", len(no_heuristic_time_to_guess))
+    print ("Number of games with correct guess: ", correct_guesses)
+    print ("Number of games with no correct guesses", no_correct_guesses)
+    print ("% Successful Games: ", round(correct_guesses / (correct_guesses + no_correct_guesses) * 100, 2 ) )
+
+'''    
     heuristic = 'word'
     word_time_to_guess = []
     for word in test_list:
         agent = Agent(agent_list)
         game = WordleGame(word)
-        constraints = None
+        constraints = []
         while game.guesses_remaining and not game.correct_guess:
             guess = agent.guess(constraints, heuristic)
             constraints = game.check_guess(guess)
@@ -54,6 +73,6 @@ if __name__ == '__main__':
             (STARTING_GUESSES + (not game.correct_guess)) 
             - game.guesses_remaining
         )
-
+'''
 
 
